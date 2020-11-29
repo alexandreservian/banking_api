1322d05bb507ee2d879a9f09097ab28189547afd
alexandreservian <alexandreservian@gmail.com>
2020-11-29 12:07:15 -0300

    chore: Adicionado ex_doc

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule BankingApi.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:bcrypt_elixir, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false}
     ]
   end
 
