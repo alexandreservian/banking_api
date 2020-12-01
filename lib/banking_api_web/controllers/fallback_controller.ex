0d441ec3bdf4b966c3e37d16d2466733f737edc1
alexandreservian <alexandreservian@gmail.com>
2020-12-01 20:33:29 -0300

    Criando view para lidar com erros com mensagens e atoms definidas pelo Contextos da aplicacao

--- lib/banking_api_web/controllers/fallback_controller.ex
+++ lib/banking_api_web/controllers/fallback_controller.ex
@@ -14,6 +14,20 @@ defmodule BankingApiWeb.FallbackController do
     |> render("error.json", changeset: changeset)
   end
 
+  def call(conn, {:error, message, :invalid_id_format}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(BankingApiWeb.ErrorView)
+    |> render("error_with_message.json", message: message)
+  end
+
+  def call(conn, {:error, message, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(BankingApiWeb.ErrorView)
+    |> render("error_with_message.json", message: message)
+  end
+
   # This clause is an example of how to handle resources that cannot be found.
   def call(conn, {:error, :not_found}) do
     conn
--- lib/banking_api_web/views/error_view.ex
+++ lib/banking_api_web/views/error_view.ex
@@ -10,6 +10,10 @@ defmodule BankingApiWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
+  def render("error_with_message.json", %{message: message}) do
+    %{message: message}
+  end
+
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
